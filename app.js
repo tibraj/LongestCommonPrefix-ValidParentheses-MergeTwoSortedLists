79f33777fb262ad42248729f86ae8c33deeb9b69
tibraj <tibraj94@gmail.com>
2021-01-12 12:13:10 -0500

    longest common prefix

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 //Longest Common Prefix
-
+function longestCommonPrefix(strings) {
+    let prefix = '';
+    if(strings.length === 0) return prefix;
+    for(let i = 0; i < strings[0].length; i++) {
+        const character = strings[0][i];
+        for(let j = 0; j < strings.length; j++) {
+            if(strings[j][i] !== character) return prefix;
+        }
+        prefix = prefix + character;
+    }
+    return prefix;
+}
 //Valid Parentheses
 
 //Merge Two Sorted Lists
