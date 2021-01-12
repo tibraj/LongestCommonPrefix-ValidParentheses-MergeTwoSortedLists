b64cbbfbdb9b04762314835b651e54bcb3dbd253
tibraj <tibraj94@gmail.com>
2021-01-12 12:20:54 -0500

    merge sorted lists

--- app.js
+++ app.js
@@ -38,3 +38,18 @@ console.log(validParentheses("([)]"))
 console.log(validParentheses("({})"))
 
 //Merge Two Sorted Lists
+function mergeSortedLists(list1, list2) {
+    if(list1 === null) {
+        return list2;
+    };
+    if(list2 === null) {
+        return list1;
+    };
+    if(list1.val < list2.val) {
+        list1.next = mergeSortedLists(list1.next, list2);
+        return list1;
+    } else {
+        list2.next = mergeSortedLists(list1, list2.next);
+        return list2;
+    }
+}
